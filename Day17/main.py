ba743d38a66c05906b13b483a6807157473f0ece
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-07-15 20:25:36 +0600

    For loops

---
+++ Day17/main.py
@@ -0,0 +1,13 @@
+#For loops
+
+# name="Mahfujar"
+# for i in range(10):
+#     print(name.upper(),end=", ")
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for i in colors:
+#     print(i.upper(), end=", ")
+
+
+for i in range (2,20,2):
+    print(i)
