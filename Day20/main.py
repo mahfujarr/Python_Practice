744fba66f81c708697862c01f0d826f01a6c4b93
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-07-16 13:02:37 +0600

    Functions

---
+++ Day20/main.py
@@ -0,0 +1,26 @@
+#functions in Python
+
+def calculateGmean(a,b):        #defining the 1st function
+    mean = (a*b)/(a+b)
+    print(mean)
+    # print("Gmean of", a , " &" , b , "is: " , mean)
+
+
+def isGreater(a,b):             #defining the 2nd function
+    if (a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+
+
+a = 1
+b = 2
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+isGreater(a,b)              #using the defined function
+calculateGmean(a,b)         #using the defined function
+
+c = 4
+d = 3
+isGreater(c,d)              #using the defined function
+calculateGmean(c,d)         #using the defined function
