39642f32f749c713788a61cf7e9f94c7c8590e9f
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-07-19 13:44:41 +0600

    f-strings

---
+++ Day28/main.py
@@ -0,0 +1,15 @@
+#f-strings in python
+
+string = "My name is {} & I live in {}."
+name = "Mahfujar"
+place = "Sundarganj"
+print(string.format(name, place))
+print(f"My name is {name} & I am from {place}.") #f-string
+
+# price = 69.999
+
+txt = "For only $ {price:.2f}!"
+print(txt.format(price = 69.999))
+
+
+print(f"{2 * 30}")
