df6e08f409fb9f0a57d4830feccaa974d37c6ffe
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-08-12 12:24:12 +0600

    Seek & tell Functions

--- Day51/main.py
+++ Day51/main.py
@@ -7,10 +7,12 @@ with open('sample.txt', 'w') as f:
     # print(type(f))
 
 
+
 with open('sample.txt', 'r') as f:
+    
     print(type(f))
 
-    seek = f.seek(3)             #Move to the 10th bite of the file
+    seek = f.seek(3)             #Move to the 10th bite of the file.
     # print(f.tell())
     print(f"Seeked to : {f.tell()}")
     data = f.read(5)       #Read the next 5 bytes.
