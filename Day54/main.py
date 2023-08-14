66b7a1863a450ad5c3ea3d95b633f7170d6e6571
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-08-14 08:19:53 +0600

    is vs '==' in Python

---
+++ Day54/main.py
@@ -0,0 +1,19 @@
+# is vs '==' in Python
+a = 4
+b = "4"
+
+print(a is b)
+print(a == b)
+
+
+a = [1,2]
+b = [1,2]
+
+print(a is b)
+print(a == b)
+
+a = (1,2)
+b = (1,2)
+
+print(a is b)
+print(a == b)
