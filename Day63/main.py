87aa2db848d679fb1e777538d0ef37540cb3a591
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-08-25 21:42:38 +0600

    exercise

--- Day63/main.py
+++ Day63/main.py
@@ -1,22 +1,3 @@
-#excercise 6
+#excercise 
 
-# Library management sytem in python
-
-class Library:
-    def __init__(self):
-        self.noBooks = 0
-        self.Books = []
-    
-    def addBooks(self, Books):
-        self.Books.append(Books)
-        self.noBooks = len(self.Books)
-
-    def details(self):
-            print(f"Total number of books are {self.noBooks} & the books are")
-            for book in self.Books:
-                print(book)
-l = Library()
-b1 = l.addBooks("Book1")
-b2 = l.addBooks("Book2")
-b3 = l.addBooks("Book3")
-l.details()
+#Solution of day55
