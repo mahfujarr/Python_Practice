e1a7debb61de192451e273db06206da1c7ec2552
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-11-04 14:22:37 +0600

    Add an else function

--- Day24/main.py
+++ Day24/main.py
@@ -7,6 +7,8 @@ print(tup[0], (tup[1]), (tup[2]), (tup[3]), (tup[4]), (tup[5]), (tup[6]), (tup[7
 
 if "Mahfujar" in tup:
     print("Present")
+else:
+    print("Not found")
 
 tup2 = tup[1:5]
 print(tup2)
