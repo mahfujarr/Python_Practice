1ec26c02ac243fe066f789425ab8edbdc2edc262
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-12-12 22:38:26 -0800

    Update

--- Day13/main.py
+++ Day13/main.py
@@ -21,9 +21,15 @@ print(a.center(50))
 print(len(a.center(50)))
 print(a.count("M")) #case sensitive
 print(a.endswith("!"))
-print(a.endswith("m"))
+print(d.endswith("!"))
 print(a.find("h"))
 print(a.endswith("h",5,6))
-print(a.find("hm"))
+print(a.find("hm")) 
+'''The find() method,
+    1)finds the first occurrence of the specified value
+    2)returns -1 if the value is not found.
+    3)almost the same as the index() method,
+    the only difference is that the index() method raises an exception
+    if the value is not found.'''
 print(a.isalnum())
 print(d.isalnum())
