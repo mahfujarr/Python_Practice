76f39b91072c54b45cb96b065710c356e774df22
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-12-30 21:14:55 -0800

    Update

--- Day26/main.py
+++ Day26/main.py
@@ -2,14 +2,16 @@
 import time
 
 t =int(time.strftime("%H"))
+# t=19
 n =input("Type your name: ")
+# n="Mahfujar"
 print(t)
 
-if (t >= 0 and t < 12):
-    print("Good Morning,", n)
+if (t >= 18 ):
+    print("Good Night,", n)
 
-elif (t >= 12 and t < 18):
+elif (t >= 12):
     print("Good Afternoon,", n)
 
-elif (t >= 18 ):
-    print("Good Night,", n)
+elif (t >= 0):
+    print("Good Morning,", n)
