a2c54e5648f588680ce55c6922feb83dba62d3aa
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-12-30 21:20:50 -0800

    Update

--- Day28/main.py
+++ Day28/main.py
@@ -8,7 +8,7 @@ print(f"My name is {name} & I am from {place}.") #f-string
 
 # price = 69.999
 
-txt = "For only $ {price:.2f}!"
+txt = "For only $ {price:.2f}!" #{price:.2f} prints only 2 decimal places.
 print(txt.format(price = 69.999))
 
 
